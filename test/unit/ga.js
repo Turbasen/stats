61504eb5da44102646f4b72a57ddb84a4f767648
Håvard Ranum <havard.ranum@me.com>
2017-03-13 09:21:23 +0100

    test(ga): add IP to test request

--- test/unit/ga.js
+++ test/unit/ga.js
@@ -7,6 +7,7 @@ const ga = require('../../');
 describe('Params', () => {
   it('are retreived', done => {
     const params = ga.getRequestParams({
+      ip: '127.0.0.1',
       user: {
         type: 'token',
         key: 'abc123',
